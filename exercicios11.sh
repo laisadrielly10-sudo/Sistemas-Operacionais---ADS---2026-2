3dada8665250fe27b8b38bafb0577e2fb6c91a7f
laisadrielly10-sudo <laisadrielly10@gmail.com>
2026-09-08 12:24:13 -0300

    Update exercicios11.sh

--- exercicios11.sh
+++ exercicios11.sh
@@ -1,1 +1,20 @@
-
+echo "===== USUÁRIOS CONECTADOS ====="
+echo "1 - Listagem simples"
+echo "2 - Listagem detalhada"
+read -p "Escolha uma opção: " opcao
+ 
+case $opcao in
+    1)
+        who
+        ;;
+    2)
+        who -a
+        ;;
+    *)
+        echo "Opção inválida."
+        exit 1
+        ;;
+esac
+ 
+echo ""
+echo "Esses são os usuários atualmente conectados ao sistema."
