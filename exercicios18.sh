cf59cb29331880e49cd43d9666f5e2f9274783d3
laisadrielly10-sudo <laisadrielly10@gmail.com>
2026-09-08 12:29:44 -0300

    Create exercicios18.sh

---
+++ exercicios18.sh
@@ -0,0 +1,15 @@
+echo "===== DATA ATUAL ====="
+date
+echo ""
+ 
+echo "===== VERSÃO DO KERNEL ====="
+uname -r
+echo ""
+ 
+echo "===== CALENDÁRIO DO MÊS ATUAL ====="
+cal
+echo ""
+ 
+echo "===== TEMPO DE FUNCIONAMENTO ====="
+uptime
+ 
