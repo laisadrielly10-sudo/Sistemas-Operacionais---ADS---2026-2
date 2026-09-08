cd2262c36878f6bd63f7c11dca12f13d521a3a9f
laisadrielly10-sudo <laisadrielly10@gmail.com>
2026-09-08 12:28:36 -0300

    Create exercicios16.sh

---
+++ exercicios16.sh
@@ -0,0 +1,9 @@
+echo "Informações do Kernel"
+echo "---------------------"
+uname -a
+ 
+echo ""
+echo "Nome do kernel: $(uname -s)"
+echo "Versão do kernel: $(uname -r)"
+echo "Arquitetura: $(uname -m)"
+ 
